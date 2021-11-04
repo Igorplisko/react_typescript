6cc65f81f11049e77683c5b61ca2e1f0681d5a38
igorplisko <igorplisko95@gmail.com>
2021-11-04 18:57:17 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Card, { CardVariant } from './components/Card';
 
 const App = () => {
   return (
     <div>
-      Working
+      <Card onClick={(num) => console.log('click', num)} variant={CardVariant.outlined} width="200px" height="200px" >
+        <button>Button</button>
+      </Card>
     </div>
   );
 }
