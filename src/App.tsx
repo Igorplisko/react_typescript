efaa43663ba209cba23ff841d2e22a50cb2658b4
igorplisko <igorplisko95@gmail.com>
2021-11-05 12:27:13 +0200

    iniit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './components/Card';
-import UserList from './components/UserList';
-import { IUser } from './types/types';
+import EventsExample from './components/EventsExample';
+import List from './components/List';
+import TodoItem from './components/TodoItem';
+import UserItem from './components/UserItem';
+// import UserList from './components/UserList';
+import { ITodo, IUser } from './types/types';
 
 const App = () => {
   const [users, setUsers] = useState<IUser[]>([])
+  const [todos, setTodos] = useState<ITodo[]>([])
 
   useEffect(() => {
     fetchUsers()
+    fetchTodos()
   }, [])
 
   async function fetchUsers() {
@@ -21,16 +28,32 @@ const App = () => {
     }
   }
 
+  async function fetchTodos() {
+    try {
+      const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      setTodos(response.data)
+    } catch (e) {
+      alert(e)
+    }
+  }
 
   return (
     <div>
+      <EventsExample />
       <Card onClick={(num) => console.log('click', num)} variant={CardVariant.outlined} width="200px" height="200px" >
         <button>Button</button>
       </Card>
-      <UserList users={users} />
+      <List
+        items={users}
+        renderItem={(user: IUser) => <UserItem user={user} key={user.id} />}
+      />
+      <List
+        items={todos}
+        renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />}
+      />
     </div>
   );
-}
+};
 
 export default App;
 
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from 'react';
 
 interface ListProps<T> {
-   items: [];
+   items: T[];
    renderItem: (item: T) => React.ReactNode
-
-
 }
 
 export default function List<T>(props: ListProps<T>) {
 
-
+   return (
+      <div>
+         {props.items.map(props.renderItem)}
+      </div>
+   )
 }
